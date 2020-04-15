45159abd24daf657900e374c5b0e9dbd78205b66
tim <tschartman@yahoo.com>
2020-04-15 00:56:32 -0500

    fix some styling issues

--- src/components/RecipeView.vue
+++ src/components/RecipeView.vue
@@ -19,25 +19,27 @@
         </v-form>
       </v-col>
       <v-col cols="12">
-       <h2>Recipe</h2>
+       <h2 class="display-1">Recipe</h2>
       </v-col>
-
-      <v-col cols="6">
-      <div v-if="ingredients.length > 0">
-        <h3>Ingredients:</h3>
-        <div class="py-1" v-for="(ingredient, index) in ingredients" v-bind:key="index">
-          <h4>{{ingredient}}</h4>
-        </div>
-      </div>
-      </v-col>
-      <v-col cols="6">
-      <div v-if="directions.length > 0">
-        <h3>Directions:</h3>
-        <div class="py-3" v-for="(direction, index) in directions" v-bind:key="index">
-          <h4>{{direction}}</h4>
-        </div>
-      </div>
-      </v-col>
+      <v-col cols="12" sm="6">
+        <h3 v-if="ingredients.length > 0" class="headline">Ingredients:</h3>
+        <v-container style="max-height: 600px;overflow-y-auto"
+        class="hide overflow-y-auto">
+          <div class="py-1" v-for="(ingredient, index) in ingredients" v-bind:key="index">
+            <h4>{{ingredient}}</h4>
+          </div>
+        </v-container>
+        </v-col>
+        <v-col style="max-height: 600px;"
+        class="hide overflow-y-auto" cols="12" sm="6">
+          <h3 v-if="directions.length > 0" class="headline">Directions:</h3>
+          <v-container style="max-height: 600px;overflow-y-auto"
+          class="hide overflow-y-auto">
+          <div class="py-3" v-for="(direction, index) in directions" v-bind:key="index">
+            <h4>{{direction}}</h4>
+          </div>
+          </v-container>
+        </v-col>
     </v-row>
   </v-container>
 </template>
@@ -64,3 +66,13 @@
     }
   }
 </script>
+<style scoped>
+.hide::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE and Edge */
+.hide {
+  -ms-overflow-style: none;
+}
+</style>
